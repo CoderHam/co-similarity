0ebdc47acda5339c9a56f9fa79330f3f48ba834a
CoderHam <hemanham@gmail.com>
2022-07-20 01:19:10 +0000

    Store numpy as as npy files instead of in a csv - Converting to csv changes array to a string

--- extract_embedding.py
+++ extract_embedding.py
@@ -13,25 +13,16 @@ API_KEY = os.getenv('COHERE_API_KEY')
 co = cohere.Client(API_KEY)
 
 def extract_embedding(sentences: List[str]):
-    response = co.embed(
-        model='small',
-        texts=sentences)
+    response = co.embed(model='small', texts=sentences)
     
     return response.embeddings
 
+# Extract sentence embeddings and store them in .npy files
 records = pd.read_csv("bgg_2000.csv")
-sentence_embeddings = None
 progress_bar = tqdm(records.iterrows())
 for idx, record in progress_bar:
     progress_bar.set_description("Extracting embedding for game %s" % idx)
     description = record['description']
     sentences = tokenize.sent_tokenize(description)
     embeddings = extract_embedding(sentences)
-    df = pd.DataFrame({'uid': idx, 'sentence': sentences, 'embed': [np.array(embedding) for embedding in embeddings]})
-    if sentence_embeddings is None:
-        sentence_embeddings = df
-    else:
-        sentence_embeddings = pd.concat([sentence_embeddings, df])
-
-# Save all sentence embeddings to file
-sentence_embeddings.to_csv("bgg_2000_embed.csv", index=False)
+    embeddings = np.save('%s.npy' % idx, embeddings)
